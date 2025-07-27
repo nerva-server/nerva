4d8c2ba6c0d31d153f835bc2353a8cd16d507e80
Wyrex <uchihasasukedev@gmail.com>
2025-07-27 20:47:25 +0300

    Refactor ThreadSafeQueue: Clean up comments and improve code clarity

--- src/Utils/ThreadSafeQueue.hpp
+++ src/Utils/ThreadSafeQueue.hpp
@@ -5,10 +5,8 @@
 #include <mutex>
 #include <condition_variable>
 #include <chrono>
-#include <atomic> // For shutdownServer flag
+#include <atomic>
 
-// Forward declaration of the global shutdown flag defined in main.cpp.
-// Each process will have its own copy of this global variable.
 extern std::atomic<bool> shutdownServer;
 
 class ThreadSafeQueue
@@ -28,20 +26,18 @@ public:
     bool pop(int &socket)
     {
         std::unique_lock<std::mutex> lock(mtx);
-        // Wait for an item or for the shutdown signal.
-        // The `shutdownServer` flag here refers to the copy in the current process.
         if (cv.wait_for(lock, std::chrono::milliseconds(100),
                         [this]
                         { return !queue.empty() || shutdownServer; }))
         {
             if (queue.empty())
-                return false; // If woken by shutdown and queue is empty
+                return false;
             socket = queue.front();
             queue.pop();
             return true;
         }
-        return false; // Timeout or shutdown
+        return false;
     }
 };
 
-#endif // THREAD_SAFE_QUEUE_HPP
+#endif

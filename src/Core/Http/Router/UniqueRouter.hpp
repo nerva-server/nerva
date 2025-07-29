28ddd6580f73db16d81c877a768ee8bf1aa28259
Naifcan Başçı <naifcanbasci@gmail.com>
2025-07-29 15:05:42 +0300

    Add UniqueRouter class

---
+++ src/Core/Http/Router/UniqueRouter.hpp
@@ -0,0 +1,14 @@
+#include "Router.hpp"
+#include <string>
+
+class Router;
+
+class UniqueRouter {
+	std::string request_type;
+	Router *r;
+
+public:
+	UniqueRouter( std::string request_type, Router *r);
+
+	void Use(const std::string &path, std::vector<std::reference_wrapper<IHandler>> middlewares, const RequestHandler &handler);
+};
